05b2aa28382691a073381827f3e1f7eebb20021f
AlpanaLonare <132905456+AlpanaLonare@users.noreply.github.com>
2023-07-16 13:09:01 +0530

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter a number: ");
+                double number = double.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new Exception("Cannot calculate square root of a negative number.");
+                }
+
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine($"The square root of {number} is: {squareRoot}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(" Please enter a valid number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
